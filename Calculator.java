222ac7e35e0cbe3b287a8d5f141da8c24ed614e9
sheteon111 <107829612+sheteon111@users.noreply.github.com>
2022-06-30 10:41:32 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,57 +3,76 @@ package assignment1;
 import java.util.Scanner;
 
 public class Calculator {
-
-	public static void main(String args[])
+	
+	private int X,Y,R;
+	
+	public void init(int X,int Y)
 	{
-		Scanner s=new Scanner(System.in);
-		int n1,n2;
-		int ans=1,c=0;
-		char choice;
-		
-		do
+		this.X=X;
+		this.Y=Y;
+	}
+	public void add()
+	{
+		R = X + Y;
+	}
+	public void multiply()
+	{
+		R = X * Y;
+	}
+	public void power()
+	{
+		R = 1;
+		while(Y!=0)
 		{
-			System.out.println( "Enter any two numbers:");
-			n1=s.nextInt();
-			n2=s.nextInt();
-			System.out.println("a. Addition\nb. Subtraction\nc. Multiplication\nd. Division\ne. Exit");
-			System.out.println("Enter Your Choice: ");
-			choice=s.next().charAt(0);
-			
-			switch(choice)
-			{
-			case 'a':
-				c=n1+n2;
-				System.out.println("Addition of two numbers is: "+c);
-				break;
-			case 'b':
-				if(n1>n2)
-					c=n1+n2;
-				else
-					c=n2-n1;
-				System.out.println("Subtraction of two numbers is: "+c);
-				break;
-			case 'c':
-				c=n1*n2;
-				System.out.println("Multiplication of two numbers is: "+c);
-				break;
-			case 'd':
-				if(n2==0)
-					System.out.println("Denominator should not be zero");
-				else
-				{
-					c=n1/n2;
-					System.out.println("Division of two numbers is: "+c);
-				}
-				break;
-			case 'e':
-				System.exit(0);
-				break;
-			default:
-				System.out.println("Wrong Choice");
-			}
-			System.out.println("Do you want to continue(1/0)?");
-			ans=s.nextInt();
-		}while(ans!=0);
+			R*=X;
+			--Y;
+		}
+	}
+	public void mod()
+	{
+		R = X % Y;
+	}
+	
+	public void display()
+	{
+		System.out.println("Result "+R);
+	}
+
+	public static void main(String[] args) {
+		int n=0;
+		Calculator m1 = new Calculator();
+		Scanner s = new Scanner(System.in);
+		//System.out.println("Enter X and Y");
+		//int x = s.nextInt();
+		//int y = s.nextInt();
+		int x = 5;
+		int y = 4;
+		m1.init(x, y);
+		//do{
+			//System.out.println("Enter Your Choice\n1.Addition 2.Multiplication 3.Power 0.Exit");
+			//n = s.nextInt();
+			//switch(n)
+			//{
+			//case 0:
+			//	n=0;
+			//	break;
+			//case 1:
+				m1.add();
+				m1.display();
+			//	break;
+			//case 2:
+				m1.multiply();
+				m1.display();
+			//	break;
+			//case 3:
+				m1.power();
+				m1.display();
+			//	break;
+				
+			//default:
+			//	System.out.println("Invalid Input");
+			//}
+		//}while(n!=0);
+		
 	}
 }
